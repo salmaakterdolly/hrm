fff6d41ee855fd4250d34ca2ed9bdbb948338b16
SALMA AKTER <akter.myself85@gmail.com>
2019-01-17 09:55:15 -0500

    https://github.com/salmaakterdolly/hrm.git

--- src/test/java/com/os/steps/Admin.java
+++ src/test/java/com/os/steps/Admin.java
@@ -3,9 +3,9 @@ package com.os.steps;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import com.os.common.userBase;
