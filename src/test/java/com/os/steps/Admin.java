034731623357e17d6e72826cb5d315e2e2bb9748
SALMA AKTER <akter.myself85@gmail.com>
2019-01-17 09:52:22 -0500

    https://github.com/salmaakterdolly/hrm.git

--- src/test/java/com/os/steps/Admin.java
+++ src/test/java/com/os/steps/Admin.java
@@ -3,42 +3,45 @@ package com.os.steps;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import com.os.common.userBase;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
+//import cucumber.api.java.After;
+//import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Admin extends userBase{
 	
-	@Before
-	public void openbrowser() {
-		launchBrowser();
-	}//end of before hooks
+	//@Before
+	//public void openbrowser() {
+		//launchBrowser();
+	//}//end of before hooks
 	
-	@After
-	public void closebrowser() {
-		tearDown();
-	}//end of After hooks
+	//@After
+	//public void closebrowser() {
+		//tearDown();
+	//}//end of After hooks
 	
 	
 	
 
 	@Given("^as a user i am on admin page$")
 	public void as_a_user_i_am_on_admin_page() throws Throwable {
+		
 	  
 	}
 
 	@When("^i clicked on admin$")
 	public void i_clicked_on_admin() throws Throwable {
 	Actions action = new Actions(driver);
-	WebElement we = driver.findElement(By.xpath("/index.php/admin/viewAdminModule"));
-	action.moveToElement(we).moveToElement(driver.findElement(By.xpath("/index.php/admin/viewAdminModule"))).click().build().perform();
+	WebElement we = driver.findElement(By.id("firstlevelmenu"));
+	action.moveToElement(we).moveToElement(driver.findElement(By.id("firstlevelmenu"))).click().build().perform();
    
     
     
@@ -47,12 +50,12 @@ public class Admin extends userBase{
 
 	@When("^i clicked on user managment$")
 	public void i_clicked_on_user_managment() throws Throwable {
-		driver.findElement(By.id("manu_admin_UserManagment")).click();  
+	  
 	}
 
 	@When("^i clicked on user$")
 	public void i_clicked_on_user() throws Throwable {
-	 driver.findElement(By.id("manu_admin_viewSystemUsers")).click();  
+	 
 	}
 
 	@Then("^i saw an error occured$")
