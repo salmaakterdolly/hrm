86e5f951dd58d228cd4bf8a30bbb2ca15255c405
SALMA AKTER <akter.myself85@gmail.com>
2019-01-22 11:44:10 -0500

    https://github.com/salmaakterdolly/hrm.git

---
+++ Features/dashboard.feature
@@ -0,0 +1,10 @@
+@Dashboard
+Feature: As a user
+I want to see the dashboard
+so that i can see the whole webpage activity
+Scenario: i am on oranization dashboard
+Given after login i can see the dashboard
+When i click on leave list
+And i click on search  
+Then i see the employee leave list page
+
--- src/test/java/com/os/steps/Admin.java
+++ src/test/java/com/os/steps/Admin.java
@@ -2,13 +2,17 @@ package com.os.steps;
 
 
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import com.os.common.userBase;
+
+//import com.os.common.userBase;
 
 //import cucumber.api.java.After;
 //import cucumber.api.java.Before;
@@ -16,7 +20,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class Admin extends userBase{
+public class Admin extends userBase {
 	
 	//@Before
 	//public void openbrowser() {
@@ -24,24 +28,22 @@ public class Admin extends userBase{
 	//}//end of before hooks
 	
 	//@After
-	//public void closebrowser() {
-		//tearDown();
+	//public void close() {
+		//CloseBrowser();
 	//}//end of After hooks
 	
 	
 	
-
 	@Given("^as a user i am on admin page$")
 	public void as_a_user_i_am_on_admin_page() throws Throwable {
-		
-	  
+		launchBrowser();  
 	}
 
 	@When("^i clicked on admin$")
 	public void i_clicked_on_admin() throws Throwable {
 	Actions action = new Actions(driver);
-	WebElement we = driver.findElement(By.id("firstlevelmenu"));
-	action.moveToElement(we).moveToElement(driver.findElement(By.id("firstlevelmenu"))).click().build().perform();
+	WebElement we = driver.findElement(By.id("firstLevelMenu"));
+    action.moveToElement(we).moveToElement(driver.findElement(By.id("firstLevelMenu"))).click().build().perform();
    
     
     
